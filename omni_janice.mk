d59f0231bf72be56bdd9272980ecdfd19f8170e7
Tesla-MADAL93 <tesla-madal93@gmail.com>
2014-04-11 15:59:33 +0200

    Fix stupid error in Omni janice device tree

--- omni_janice.mk
+++ omni_janice.mk
@@ -1,5 +1,8 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit Omni Product configuration.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9070
@@ -8,8 +11,6 @@ PRODUCT_RELEASE_NAME := GT-I9070
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/janice/full_janice.mk)
@@ -22,7 +23,7 @@ PRODUCT_PACKAGES += \
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := janice
-PRODUCT_NAME := cm_janice
+PRODUCT_NAME := omni_janice
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9070
 PRODUCT_MANUFACTURER := samsung
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo omni_janice-eng
+add_lunch_combo omni_janice-userdebug
